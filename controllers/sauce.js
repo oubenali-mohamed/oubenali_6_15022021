2e1083db1897fd87eba5c8a3a1c0a5ee1d88815e
oubenali mohamed <oubenali2010@hotmail.fr>
2021-02-28 23:08:31 +0100

    modification des middleware suppression et modification des sauces

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -4,11 +4,11 @@ const fs = require('fs');
 exports.createSauce =  (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);
     delete sauceObject._id
-    const sauce = new sauce({
+    const sauce = new Sauce({
         ...sauceObject,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
-    Sauce.save()
+    sauce.save()
     .then(() => res.status(201).json({message: 'sauce enregistré'}))
     .catch(error => res.status(400).json({error}))
 };
@@ -17,9 +17,9 @@ exports.modifySauce =  (req, res, next) => {
     const sauceObject = req.file ?
     {
         ...JSON.parse(req.body.sauce),
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    } : {...req.body}
-    Sauce.updateOne({_id: req.params.id}, {sauceObject, _id: req.params.id})
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+    } : {...req.body};
+    Sauce.updateOne({_id: req.params.id}, {...sauceObject, _id: req.params.id})
     .then(() => res.status(200).json({message: 'Sauce modifiée'}))
     .catch(error => res.status().json({error}))
 };
@@ -29,24 +29,24 @@ exports.deleteSauce =  (req, res, next) => {
     .then(sauce => {
         const filename = sauce.imageUrl.split('/images/')[1];
         fs.unlink(`images/${filename}`, () => {
-            Sauce.deleteOne({_id: req.param.id})
+            Sauce.deleteOne({_id: req.params.id})
             .then(() => res.status(200).json({message: 'Sauce supprimée'}))
             .catch(error => res.status(400).json({error}))
         });
     })
-    .catch(error => res.status(500).json({error}))
+    .catch(error=> res.status(500).json({error}))
    
 };
 
 exports.getOneSauce =  (req, res, next) => {
     Sauce.findOne({_id: req.params.id})
-    .then(sauce => res.status(200).json({sauce}))
+    .then(sauce => res.status(200).json(sauce))
     .catch(error => res.status(404).json({error}))
 };
 
 exports.getAllSauces =  (req, res, next) => {
     Sauce.find()
     .then(sauces => res.status(200).json(sauces))
-    .catch(error => res.status(400).json({error}));
+    .catch(error=> res.status(400).json({error}));
  };
 
