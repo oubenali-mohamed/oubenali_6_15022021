ff24a55b90b37192c0a259eb7e0990eed237df94
oubenali mohamed <oubenali2010@hotmail.fr>
2021-03-06 08:50:07 +0100

    ajout du fichier .env pour les variables d'environnement

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
-
+const helmet = require('helmet');
+const session = require('express-session');
+const emailValidator = require('email-validator');
+const dotenv = require('dotenv').config();
+//const passwordValidator = require('password-validator');
+ 
 const sauceRoutes = require('./routes/Sauce')
 const userRoutes = require('./routes/user')
 const app = express();
 
-mongoose.connect('mongodb+srv://ouben070986:Isuino860907@cluster0.zpbkz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
+mongoose.connect(
+  { 
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+  })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
@@ -20,8 +30,19 @@ mongoose.connect('mongodb+srv://ouben070986:Isuino860907@cluster0.zpbkz.mongodb.
     next();
   });
 
-app.use(bodyParser.json());
 
+emailValidator.validate('mail121-_gmail.com')
+ 
+app.use(bodyParser.json());
+app.use(helmet());
+
+ app.set('trust proxy', 1);
+app.use(session({
+  secret: 's3Cur3',
+  name: 'sessionID',
+  resave: false,
+  saveUninitialized: true
+})); 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/api/sauces', sauceRoutes);
